34d287201c32a7847963e9a42693bed087ad1ce9
wahidabd <wahed.blog99@gmail.com>
2022-03-28 22:29:55 +0700

    change id to class komik/hot

--- routers/manga.js
+++ routers/manga.js
@@ -440,7 +440,7 @@ router.get("/komik/hot", async(req, res) => {
   try{
     const reponse = await AxiosService();
     const $ = cheerio.load(reponse.data);
-    const element = $("#Komik_Hot > .perapih");
+    const element = $(".ls > .perapih");
     let thumb, title, endpoint, type, upload_on, views, genre, chapter, warna;
     let manga_list = [];
 
